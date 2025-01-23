e7834f1fc17ca76e7cd002d00d8246da3c94f0d6
jjawang <chadae111@naver.com>
2025-01-23 14:08:55 +0900

    start project

---
+++ boll.js
@@ -0,0 +1,20 @@
+const doll = document.querySelector('.doll')
+
+let count = 0;
+
+for(let i = 0; i < 361; i++){
+    doll.innerHTML += `<i></i>`
+}
+
+const ac = document.querySelectorAll('i')
+
+ac.forEach( e =>{
+    e.addEventListener('click',()=>{
+        if(count % 2 === 0){
+            e.innerHTML = `<div class="boduc black"></div>`
+        }else{
+            e.innerHTML = `<div class="boduc white"></div>`
+        }
+        count++
+    })
+})
